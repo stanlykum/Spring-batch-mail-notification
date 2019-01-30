44089955ba8bbce6cc95eda6f385b48287abd383
unknown <stanly.kum@gmail.com>
2019-01-31 02:14:35 +0530

    Initial commit

---
+++ src/main/java/com/ajtech/mail/batch/SpringBatchMailNotification.java
@@ -0,0 +1,16 @@
+package com.ajtech.mail.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class SpringBatchMailNotification {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchMailNotification.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/ajtech/mail/batch/Springbatchmailnotification/SpringBatchMailNotificationApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ajtech.mail.batch.Springbatchmailnotification;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBatchMailNotificationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
